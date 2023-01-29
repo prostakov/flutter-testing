730b196815fa64189562a29589991b92f956a371
Serhii Prostakov <serhii.prostrakov@gmail.com>
2023-01-30 00:23:59 +0200

    Fixing Great Places implementation

---
+++ great_places_app/lib/helpers/location_helper.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const GOOGLE_API_KEY = 'AIzaSyBg9yn5JtQgKRFbg6FCTy4ewbF24kRuAYI';
+
+class LocationHelper {
+  static String generateLocationPreviewImage({
+    required double latitude,
+    required double longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url = Uri(
+      scheme: 'https',
+      host: 'maps.googleapis.com',
+      path: 'maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY',
+    );
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
+  }
+}
