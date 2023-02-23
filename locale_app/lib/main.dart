4bedcc01c759d5169cfc28973c0e9c7efcd4ae4c
Serhii Prostakov <serhii.prostrakov@gmail.com>
2023-02-24 00:48:07 +0200

    Add implementation for locale app

--- locale_app/lib/main.dart
+++ locale_app/lib/main.dart
@@ -1,115 +1,147 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:devicelocale/devicelocale.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  List? _languages = [];
+  String? _currentLocale;
+  String? _defaultLocale;
+  bool _isSupported = false;
 
-  // This widget is the root of your application.
+  @override
+  void initState() {
+    super.initState();
+    _initPlatformState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: ListView(
+            padding: EdgeInsets.all(16),
+            children: [
+              Text("Default locale: "),
+              Text('$_defaultLocale'),
+              ElevatedButton(
+                onPressed: _getDefaultLocale,
+                child: Text("Refresh"),
+              ),
+              SizedBox(height: 8),
+              Text("Reported Current locale: "),
+              Text('$_currentLocale'),
+              ElevatedButton(
+                onPressed: _getCurrentLocale,
+                child: Text("Refresh"),
+              ),
+              SizedBox(height: 8),
+              Container(
+                color: Colors.blue,
+                height: 1,
+              ),
+              SizedBox(height: 8),
+              Text("Preferred Languages: "),
+              Text(_languages.toString()),
+              ElevatedButton(
+                onPressed: _getPreferredLanguages,
+                child: Text("Refresh"),
+              ),
+              SizedBox(height: 8),
+              Container(
+                color: Colors.blue,
+                height: 1,
+              ),
+              if (_isSupported) ...[
+                Text("Save languages: "),
+                ElevatedButton(
+                  onPressed: () => _saveLanguage(Locale("fr", "FR")),
+                  child: Text("Save fr-FR"),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  color: Colors.blue,
+                  height: 1,
+                ),
+                ElevatedButton(
+                  onPressed: () => _saveLanguage(Locale("en", "CA")),
+                  child: Text("Save en-CA"),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  color: Colors.blue,
+                  height: 1,
+                ),
+              ] else ...[
+                Text('Set language per app is not available on OS level'),
+              ],
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> _initPlatformState() async {
+    _getCurrentLocale();
+    _getDefaultLocale();
+    _getPreferredLanguages();
+    _checkForSupport();
+  }
+
+  Future<void> _getDefaultLocale() async {
+    try {
+      final defaultLocale = await Devicelocale.defaultLocale;
+      print((defaultLocale != null)
+          ? defaultLocale
+          : "Unable to get defaultLocale");
+      setState(() => _defaultLocale = defaultLocale);
+    } on PlatformException {
+      print("Error obtaining default locale");
+    }
+  }
+
+  Future<void> _getCurrentLocale() async {
+    try {
+      final currentLocale = await Devicelocale.currentLocale;
+      print((currentLocale != null)
+          ? currentLocale
+          : "Unable to get currentLocale");
+      setState(() => _currentLocale = currentLocale);
+    } on PlatformException {
+      print("Error obtaining current locale");
+    }
+  }
+
+  Future<void> _getPreferredLanguages() async {
+    try {
+      final languages = await Devicelocale.preferredLanguages;
+      print((languages != null)
+          ? languages
+          : "unable to get preferred languages");
+      setState(() => _languages = languages);
+    } on PlatformException {
+      print("Error obtaining preferred languages");
+    }
+  }
+
+  Future<void> _checkForSupport() async {
+    final isSupported = await Devicelocale.isLanguagePerAppSettingSupported;
+    setState(() => _isSupported = isSupported);
+  }
+
+  Future<void> _saveLanguage(Locale locale) async {
+    await Devicelocale.setLanguagePerApp(locale);
   }
 }
