af8ba89471b2a206d5d4c19f6002f61ae980ee23
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-13 11:53:49 +0300

    Fix cupertino02ActionSheetActionScreen.dart

--- cupertino_test/lib/screens/cupertino02ActionSheetActionScreen.dart
+++ cupertino_test/lib/screens/cupertino02ActionSheetActionScreen.dart
@@ -11,7 +11,32 @@ class _Cupertino02ActionSheetActionScreenState extends State<Cupertino02ActionSh
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoActionSheetAction'),
-      child: Center(child: Text('TODO')),
+      child: Center(
+        child: CupertinoButton(
+          child: Text('CupertinoActionSheetAction'),
+          onPressed: () {
+            showCupertinoModalPopup(
+                context: context,
+                builder: (BuildContext context) => CupertinoActionSheet(
+                      title: Text('Select option:'),
+                      actions: [
+                        CupertinoActionSheetAction(
+                          child: Text('Option 1'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        CupertinoActionSheetAction(
+                          child: Text('Option 2'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    ));
+          },
+        ),
+      ),
     );
   }
 }
