5ae256ddb1aaa09379136ad2b7884e019dc222c0
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-13 15:48:27 +0300

    Fix cupertino07DatePickerScreen.dart

--- cupertino_test/lib/screens/cupertino07DatePickerScreen.dart
+++ cupertino_test/lib/screens/cupertino07DatePickerScreen.dart
@@ -1,5 +1,6 @@
 import 'package:cupertino_test/widgets/default_header.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Cupertino07DatePickerScreen extends StatefulWidget {
   @override
@@ -7,11 +8,33 @@ class Cupertino07DatePickerScreen extends StatefulWidget {
 }
 
 class _Cupertino07DatePickerScreenState extends State<Cupertino07DatePickerScreen> {
+  DateTime dateTime = DateTime(2100, 1, 1);
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoDatePicker'),
-      child: Center(child: Text('TODO')),
+      child: Center(
+        child: CupertinoButton.filled(
+          child: Text('Select Date'),
+          onPressed: () {
+            showCupertinoModalPopup<void>(
+              context: context,
+              builder: (BuildContext context) => SizedBox(
+                height: 250,
+                child: CupertinoDatePicker(
+                  initialDateTime: dateTime,
+                  mode: CupertinoDatePickerMode.date,
+                  use24hFormat: true,
+                  onDateTimeChanged: (DateTime newDate) {
+                    setState(() => dateTime = newDate);
+                  },
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
