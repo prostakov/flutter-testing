b474ceb91b88b5339e1b215d2e55f93535abbe59
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-13 13:14:28 +0300

    Fix cupertino06ContextMenuScreen.dart

--- cupertino_test/lib/screens/cupertino06ContextMenuScreen.dart
+++ cupertino_test/lib/screens/cupertino06ContextMenuScreen.dart
@@ -11,7 +11,27 @@ class _Cupertino06ContextMenuScreenState extends State<Cupertino06ContextMenuScr
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoContextMenu'),
-      child: Center(child: Text('TODO')),
+      child: Center(
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: CupertinoContextMenu(
+            child: Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png',
+            ),
+            actions: [
+              CupertinoContextMenuAction(
+                child: Text('Action 1'),
+                onPressed: () => Navigator.pop(context),
+              ),
+              CupertinoContextMenuAction(
+                child: Text('Action 2'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
