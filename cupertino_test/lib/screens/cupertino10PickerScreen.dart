76fdaf60e6401f7d0672eb105847f3523a6c1874
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-13 19:40:12 +0300

    Fix cupertino10PickerScreen.dart

--- cupertino_test/lib/screens/cupertino10PickerScreen.dart
+++ cupertino_test/lib/screens/cupertino10PickerScreen.dart
@@ -7,11 +7,36 @@ class Cupertino10PickerScreen extends StatefulWidget {
 }
 
 class _Cupertino10PickerScreenState extends State<Cupertino10PickerScreen> {
+  int _selectedValue = 10;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoPicker'),
-      child: Center(child: Text('TODO')),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoButton.filled(
+              child: Text('Current selected value: ${_selectedValue}'),
+              onPressed: () => showCupertinoModalPopup(
+                context: context,
+                builder: (_) => SizedBox(
+                  width: double.infinity,
+                  height: 250,
+                  child: CupertinoPicker(
+                    itemExtent: 30,
+                    scrollController: FixedExtentScrollController(initialItem: _selectedValue),
+                    children: [for (var i = 0; i <= 30; i++) Text("${i}")],
+                    onSelectedItemChanged: (int value) => setState(() => _selectedValue = value),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
