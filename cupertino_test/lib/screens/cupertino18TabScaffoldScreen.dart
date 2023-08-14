bda7d940d128a7f6a6a606f497302dd3ba61a74b
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-14 16:05:04 +0300

    Fix cupertino18TabScaffoldScreen.dart

--- cupertino_test/lib/screens/cupertino18TabScaffoldScreen.dart
+++ cupertino_test/lib/screens/cupertino18TabScaffoldScreen.dart
@@ -9,9 +9,24 @@ class Cupertino18TabScaffoldScreen extends StatefulWidget {
 class _Cupertino18TabScaffoldScreenState extends State<Cupertino18TabScaffoldScreen> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: DefaultHeader(title: 'CupertinoTabScaffold'),
-      child: Center(child: Text('TODO')),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) => CupertinoTabView(
+        builder: (context) => Center(
+          child: index == 0 ? Text('Home') : Text('Settings'),
+        ),
+      ),
     );
   }
 }
