b0017db74580b8bf9710647c4ea56768b75a925e
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-14 09:52:53 +0300

    Fix cupertino13SearchTextFieldScreen.dart

--- cupertino_test/lib/screens/cupertino13SearchTextFieldScreen.dart
+++ cupertino_test/lib/screens/cupertino13SearchTextFieldScreen.dart
@@ -7,11 +7,14 @@ class Cupertino13SearchTextFieldScreen extends StatefulWidget {
 }
 
 class _Cupertino13SearchTextFieldScreenState extends State<Cupertino13SearchTextFieldScreen> {
+  final TextEditingController _textEditingController = TextEditingController(text: "Some search");
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoSearchTextField'),
-      child: Center(child: Text('TODO')),
+      child: Center(
+        child: CupertinoSearchTextField(controller: _textEditingController),
+      ),
     );
   }
 }
