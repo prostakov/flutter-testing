edbe486c55b4ad05830d0e0e846413d1faaa9e16
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-14 12:59:19 +0300

    Fix cupertino15SliderScreen.dart

--- cupertino_test/lib/screens/cupertino15SliderScreen.dart
+++ cupertino_test/lib/screens/cupertino15SliderScreen.dart
@@ -7,11 +7,29 @@ class Cupertino15SliderScreen extends StatefulWidget {
 }
 
 class _Cupertino15SliderScreenState extends State<Cupertino15SliderScreen> {
+  double _currentValue = 2;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoSlider'),
-      child: Center(child: Text('TODO')),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            Text('${_currentValue}'),
+            const SizedBox(height: 50),
+            CupertinoSlider(
+              value: _currentValue,
+              min: 0,
+              max: 10,
+              divisions: 10,
+              onChanged: (value) => setState(() => _currentValue = value),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
