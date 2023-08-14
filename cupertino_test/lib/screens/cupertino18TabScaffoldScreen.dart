2ff9740b65817e48da8a964a5f31d32e047bf50d
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-14 12:59:14 +0300

    Revert cupertino18TabScaffoldScreen.dart

--- cupertino_test/lib/screens/cupertino18TabScaffoldScreen.dart
+++ cupertino_test/lib/screens/cupertino18TabScaffoldScreen.dart
@@ -7,29 +7,11 @@ class Cupertino18TabScaffoldScreen extends StatefulWidget {
 }
 
 class _Cupertino18TabScaffoldScreenState extends State<Cupertino18TabScaffoldScreen> {
-  double _currentValue = 2;
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoTabScaffold'),
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            Text('${_currentValue}'),
-            const SizedBox(height: 50),
-            CupertinoSlider(
-              value: _currentValue,
-              min: 0,
-              max: 10,
-              divisions: 10,
-              onChanged: (value) => setState(() => _currentValue = value),
-            )
-          ],
-        ),
-      ),
+      child: Center(child: Text('TODO')),
     );
   }
 }
