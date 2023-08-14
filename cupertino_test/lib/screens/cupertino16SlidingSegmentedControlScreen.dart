f82c794a3469fe2bfd07ebf0d0e71f504c182247
Serhii Prostakov <serhii.prostakov@gmail.com>
2023-08-14 13:06:21 +0300

    Fix cupertino16SlidingSegmentedControlScreen.dart

--- cupertino_test/lib/screens/cupertino16SlidingSegmentedControlScreen.dart
+++ cupertino_test/lib/screens/cupertino16SlidingSegmentedControlScreen.dart
@@ -7,11 +7,41 @@ class Cupertino16SlidingSegmentedControlScreen extends StatefulWidget {
 }
 
 class _Cupertino16SlidingSegmentedControlScreenState extends State<Cupertino16SlidingSegmentedControlScreen> {
+  String? _currentText = null;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: DefaultHeader(title: 'CupertinoSlidingSegmentedControl'),
-      child: Center(child: Text('TODO')),
+      child: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            CupertinoSlidingSegmentedControl(
+              groupValue: _currentText,
+              backgroundColor: CupertinoTheme.of(context).barBackgroundColor,
+              thumbColor: CupertinoColors.systemOrange,
+              children: {
+                "Twitter": Container(
+                  child: Text("Twitter"),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                ),
+                "YouTube": Container(
+                  child: Text("YouTube"),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                ),
+                "Instagram": Container(
+                  child: Text("Instagram"),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                ),
+              },
+              onValueChanged: (String? value) => setState(() => _currentText = value),
+            ),
+            const SizedBox(height: 50),
+            _currentText != null ? Text(_currentText!, style: TextStyle(fontSize: 20)) : Container(),
+          ],
+        ),
+      ),
     );
   }
 }
